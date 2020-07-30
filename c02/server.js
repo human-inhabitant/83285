c5bd4e34c9ad7a3f0696c4323be5eca1baaf9b1a
Human Inhabitant <human-inhabitant@pm.me>
2020-07-30 17:01:48 -0400

    Completed chapter 2...

--- c02/server.js
+++ c02/server.js
@@ -1,2 +1,24 @@
-const hello = require( './hello' );
-hello();
+const connect = require( 'connect' );
+const app = connect();
+
+const logger = function( req, res, next ) {
+  console.info( req.method, req.url );
+  next();
+};
+app.use( logger );
+
+const wurd = function( req, res, next ) {
+  res.setHeader( 'Content-Type', 'text/plain' );
+  res.end( 'Wurd...' );
+};
+app.use( '/wurd', wurd );
+
+const wurdNot = function( req, res, next ) {
+  res.setHeader( 'Content-Type', 'text/plain' );
+  res.end( 'Wurd... NOT!' );
+};
+app.use( '/not', wurdNot );
+
+app.listen( 3000 );
+
+console.info('Server running at http://localhost:3000/' );
